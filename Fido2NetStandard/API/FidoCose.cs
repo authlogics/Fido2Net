b40f26457f67821fb64e9d2180c1035cf4e3ff54
James Westgate <jamesw@authlogics.com>
2023-09-18 14:47:48 +0100

    Add .net standard version

---
+++ Fido2NetStandard/API/FidoCose.cs
@@ -0,0 +1,26 @@
+﻿namespace Fido2Net
+{
+    /// <summary>
+    /// CBOR Object Signing and Encryption algorithms (RFC 8152)
+    /// </summary>
+    public enum FidoCose
+    {
+        Unspecified = 0,
+
+        /// <summary>
+        /// ECDSA w/ SHA-256
+        /// </summary>
+        ES256 = -7,
+
+        EDDSA = -8,
+
+        ECDH_ES256 = -25,
+
+        /// <summary>
+        /// RSA Signature with SHA-256
+        /// </summary>
+        RS256 = -257,
+
+        RS1 = -65536
+    }
+}
---
+++ Fido2NetStandard/API/FidoExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Fido2Net
+{
+    /// <summary>
+    /// Extensions for a FIDO2 device
+    /// </summary>
+    public enum FidoExtensions
+    {
+        /// <summary>
+        /// No extensions
+        /// </summary>
+        None = 0x00,
+
+        /// <summary>
+        /// HMAC Secret extension (CTAP §9.1)  
+        /// </summary>
+        HmacSecret = 0x01,
+
+        CredentialProtection = 0x02,
+        LargeBlobKey = 0x04,
+        CredentialBlob = 0x08,
+        MinimumPinLength = 0x10
+    }
+}
